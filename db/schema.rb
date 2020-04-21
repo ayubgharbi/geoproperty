770c3254bd4ef6bdb3021ebeabbaa3ab6dbcf3eb
ayoub gharbi <ayoub97ma@gmail.com>
2020-04-21 20:24:32 +0000

    generate migration for parking spaces and customize property show view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200419052406) do
+ActiveRecord::Schema.define(version: 20200421193005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20200419052406) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parking_spaces", default: 0
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
