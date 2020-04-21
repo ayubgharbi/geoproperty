5aa376dfca5bd8062c23adc81c273e29b925419a
ayoub gharbi <ayoub97ma@gmail.com>
2020-04-21 13:52:59 +0000

    create set_sidebar method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   get "/dashboard" => 'dashboard#index', as: :dashboard
 
-  get 'dashboard/properties'
-
-  get 'dashboard/reports'
-
   resources :properties
   devise_for :accounts
   root 'public#main'
