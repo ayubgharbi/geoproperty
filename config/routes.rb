9a456d417a1a1482f9c8d62c34b9c0c37cb19fe3
ayoub gharbi <ayoub97ma@gmail.com>
2020-04-28 04:31:47 +0000

    add profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get "/dashboard" => 'dashboard#index', as: :dashboard
-
-  resources :properties
   devise_for :accounts
-  root 'public#main'
+  resources :properties
 
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get "/profile/:id" => 'dashboard#profile', as: :profile
+
+  
+  
+  root 'public#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
