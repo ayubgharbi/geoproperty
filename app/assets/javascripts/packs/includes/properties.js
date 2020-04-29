ad2d3b6fe3e5b0afa5f0c402682f98f998034ebf
ayoub gharbi <ayoub97ma@gmail.com>
2020-04-29 02:34:15 +0000

    show more & less button

--- app/assets/javascripts/packs/includes/properties.js
+++ app/assets/javascripts/packs/includes/properties.js
@@ -4,5 +4,10 @@ $(function(){
 
         console.log("clicked");
         details.toggleClass("open");
-    })
+        if(details.hasClass("open")){
+            $(this).text("Show less");
+        } else {
+            $(this.text("Show more"));
+        }
+    });
 });
