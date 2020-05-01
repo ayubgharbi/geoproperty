9c79241f6a97fb2c0db1147971bfedad8cca143f
ayoub gharbi <ayoub97ma@gmail.com>
2020-05-01 22:51:47 +0000

    show number inside call button

--- app/assets/javascripts/packs/includes/properties.js
+++ app/assets/javascripts/packs/includes/properties.js
@@ -1,4 +1,8 @@
 $(function(){
+    $("#show-tel").on("click", function(){
+    var $this = $(this);
+    $this.find("span").text( $this.data("telephone") );
+    });
     $("#toggleDetails").on("click", function(){
         var details = $("#prop-details");
         details.toggleClass("open");
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200430094119) do
+ActiveRecord::Schema.define(version: 20200501203638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20200430094119) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "company"
+    t.string "telephone"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
